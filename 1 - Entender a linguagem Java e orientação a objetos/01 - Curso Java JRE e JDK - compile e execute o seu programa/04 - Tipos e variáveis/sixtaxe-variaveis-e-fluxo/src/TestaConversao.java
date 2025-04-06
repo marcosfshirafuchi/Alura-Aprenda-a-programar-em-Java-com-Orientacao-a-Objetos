87bbf7a50699ad7eee2708f11441a23a1e9d3799
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-04-06 19:50:58 -0300

    12 Arquivos do projeto atual

--- 1 - Entender a linguagem Java e orientação a objetos/01 - Curso Java JRE e JDK - compile e execute o seu programa/04 - Tipos e variáveis/sixtaxe-variaveis-e-fluxo/src/TestaConversao.java
+++ 1 - Entender a linguagem Java e orientação a objetos/01 - Curso Java JRE e JDK - compile e execute o seu programa/04 - Tipos e variáveis/sixtaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -9,6 +9,7 @@ public class TestaConversao {
         double valor1 = 0.2;
         double valor2 = 0.1;
         double total = valor1+valor2;
+        System.out.println(total);
         float pontoFlutuante = 3.14f;
     }
 }
