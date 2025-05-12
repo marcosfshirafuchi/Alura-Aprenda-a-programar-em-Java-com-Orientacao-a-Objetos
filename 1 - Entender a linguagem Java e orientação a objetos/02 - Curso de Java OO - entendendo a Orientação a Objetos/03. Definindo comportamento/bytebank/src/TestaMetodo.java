c01418c0518ef359138a26172a8fe5332bfd2908
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-05-11 21:49:00 -0300

    03. Definindo comportamento - 05 Métodos com retorno

--- 1 - Entender a linguagem Java e orientação a objetos/02 - Curso de Java OO - entendendo a Orientação a Objetos/03. Definindo comportamento/bytebank/src/TestaMetodo.java
+++ 1 - Entender a linguagem Java e orientação a objetos/02 - Curso de Java OO - entendendo a Orientação a Objetos/03. Definindo comportamento/bytebank/src/TestaMetodo.java
@@ -4,6 +4,8 @@ public class TestaMetodo {
         contaDoPaulo.saldo = 100;
         contaDoPaulo.deposita(50);
         System.out.println(contaDoPaulo.saldo);
-
+        boolean conseguiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conseguiuRetirar);
     }
 }
