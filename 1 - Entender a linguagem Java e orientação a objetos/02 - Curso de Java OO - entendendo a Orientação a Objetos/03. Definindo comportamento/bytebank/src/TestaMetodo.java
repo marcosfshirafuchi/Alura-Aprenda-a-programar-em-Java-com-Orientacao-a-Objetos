6ef2ea058a079b8f9bdd270ee52098a33807fe9d
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-05-16 21:24:22 -0300

    08 Métodos com referência e mais retorno

--- 1 - Entender a linguagem Java e orientação a objetos/02 - Curso de Java OO - entendendo a Orientação a Objetos/03. Definindo comportamento/bytebank/src/TestaMetodo.java
+++ 1 - Entender a linguagem Java e orientação a objetos/02 - Curso de Java OO - entendendo a Orientação a Objetos/03. Definindo comportamento/bytebank/src/TestaMetodo.java
@@ -7,5 +7,15 @@ public class TestaMetodo {
         boolean conseguiuRetirar = contaDoPaulo.saca(20);
         System.out.println(contaDoPaulo.saldo);
         System.out.println(conseguiuRetirar);
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.deposita(1000);
+        boolean sucessoTransferencia = contaDaMarcela.transfere(3000,contaDoPaulo);
+        if (sucessoTransferencia){
+            System.out.println("transferencia realizada com sucesso");
+        }else{
+            System.out.println("faltou dinheiro");
+        }
+        System.out.println(contaDaMarcela.saldo);
+        System.out.println(contaDoPaulo.saldo);
     }
 }
